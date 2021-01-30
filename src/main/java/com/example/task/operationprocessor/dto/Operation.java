2e35d81907f85f11c0c325bb56fd3ed28ff75cde
jonjua_rm <jonjua_rm@taximaxim.ru>
2021-01-30 14:39:16 +0300

    jackson config

--- src/main/java/com/example/task/operationprocessor/dto/Operation.java
+++ src/main/java/com/example/task/operationprocessor/dto/Operation.java
@@ -1,16 +1,20 @@
 package com.example.task.operationprocessor.dto;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Operation {
     private Integer accountNumber;
     private Integer recipientNumber;
     private String ip;
     private Integer amount;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime date;
 }
