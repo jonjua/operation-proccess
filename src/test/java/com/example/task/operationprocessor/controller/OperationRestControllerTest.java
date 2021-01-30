60d5c12d507fbc96c10e5dea52a9301392fbf48c
jonjua_rm <jonjua_rm@taximaxim.ru>
2021-01-30 14:39:54 +0300

    jackson config

---
+++ src/test/java/com/example/task/operationprocessor/controller/OperationRestControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.task.operationprocessor.controller;
+
+
+import com.example.task.operationprocessor.OperationProcessorApplication;
+import com.example.task.operationprocessor.dto.Operation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = OperationProcessorApplication.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+
+public class OperationRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void addTwoIdenticalOperation_ReturnListOfErrorStatus() throws Exception {
+
+
+
+        this.mockMvc.perform(
+                post("/api/operation/add")
+                        .param("accountNumber", "1")
+                        .param("recipientNumber", "2")
+                        .param("ip", "192.168.0.1")
+                        .param("amount", "100")
+
+                        .param("date", "2020-12-03 01:15:30")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"))
+                .andReturn();
+
+        this.mockMvc.perform(
+                post("/api/operation/add")
+                        .param("accountNumber", "1")
+                        .param("recipientNumber", "2")
+                        .param("ip", "192.168.0.1")
+                        .param("amount", "100")
+
+                        .param("date", "2020-12-03 01:15:30")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"))
+                .andReturn();
+
+    }
+
+}
